8ba084fdcbee1a85a769b57f53dca98b71a9f4dd
Howard <howard@email.com>
2025-03-11 22:56:32 +0800

    update test_devices.py

--- test_devices.py
+++ test_devices.py
@@ -2,10 +2,19 @@ import pyaudio
 p = pyaudio.PyAudio()
 devices_num  = p.get_device_count()
 
-devices_list = []
+input_devices_list = []
+output_devices_list = []
 
 for i in range(devices_num):
     device  =  p.get_device_info_by_index(i)
-    devices_list.append(device)
-for device in devices_list:
+    if(device['maxInputChannels'] > 0):
+        input_devices_list.append(device)
+    elif(device['maxOutputChannels'] > 0):
+        output_devices_list.append(device)
+print("input devices list:")
+for device in input_devices_list:
+    print(device)
+
+print("output devices list:")
+for device in output_devices_list:
     print(device)
