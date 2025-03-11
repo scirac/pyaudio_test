e151fc570e0f3459da9fc505532512fb9c929ebd
Howard <howard@email.com>
2025-03-11 15:53:32 +0800

    add test_record_new

---
+++ test_record_new.py
@@ -0,0 +1,34 @@
+import wave
+import numpy as np
+import pyaudio
+
+SAMPLE_RATE = 32000
+FRAME_LENGTH_MS = 20
+CHUNK = SAMPLE_RATE // 1000 * FRAME_LENGTH_MS
+FORMAT = pyaudio.paInt16
+CHANNELS = 8
+
+RECORD_SECONDS = 20
+
+wf = wave.open('record.wav','wb')
+wf.setnchannels(CHANNELS)
+wf.setsampwidth(2)
+wf.setframerate(SAMPLE_RATE)
+
+try:
+    p = pyaudio.PyAudio()
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=SAMPLE_RATE,
+                    input=True,
+                    )
+
+    print(f'Recording: sample_rate:{SAMPLE_RATE}, channels:{CHANNELS}, time:{RECORD_SECONDS}s')
+    for _ in range(0, SAMPLE_RATE // CHUNK * RECORD_SECONDS):
+        data = stream.read(CHUNK)
+        wf.writeframes(data)
+    print('Done')
+finally:
+    stream.close()
+    p.terminate()
+    wf.close()
